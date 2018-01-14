efe1d15fec3cfbe945a1236bdd6ed709b1f7151e
nnchen <32369145+nnchen@users.noreply.github.com>
2018-01-14 01:22:30 -0800

    Add files via upload

---
+++ search_button.java
@@ -0,0 +1,20 @@
+package homework_week9;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class search_button {
+
+		WebDriver driver = null;
+	
+	public void searchButton() throws Throwable {
+		
+		driver = new ChromeDriver();
+		
+		driver.findElement(By.name("submit_search")).click();
+		
+		
+	}
+	
+}
